73ab2217292cb11b6bb417fc4ec6d7b1a6d2bb5d
Irma <113047558+irmabrr@users.noreply.github.com>
2025-11-06 12:38:44 -0500

    Add files via upload

---
+++ quad.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+  
+  double a; 
+  double b;
+  double c;
+
+  std::cout << "Enter a:";
+  std::cin >> a;
+
+  std::cout << "Enter b:";
+  std::cin >> b;
+
+  std::cout << "Enter c";
+  std::cin >> c;
+  
+  double root1 = (-b + std::sqrt(b*b - 4*a*c)) / (2*a);
+  double root2 = (-b - std::sqrt(b*b - 4*a*c)) / (2*a);
+
+  std::cout << root1 << root2;
+}
